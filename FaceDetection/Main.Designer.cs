fcd96aeaa4e65b10e5487a63b578d11563e931fd
Kevin Zulkarnain Yuseti <kevinyuseti@yahoo.com>
2016-05-13 18:02:48 +0700

    initial push udah ada ambil gambar sama filter pilihan gambar

---
+++ FaceDetection/Main.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace FaceDetection
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Asal = new System.Windows.Forms.PictureBox();
+            this.Hasil = new System.Windows.Forms.PictureBox();
+            this.pilihGambar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Asal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Hasil)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Asal
+            // 
+            this.Asal.Location = new System.Drawing.Point(12, 12);
+            this.Asal.Name = "Asal";
+            this.Asal.Size = new System.Drawing.Size(300, 300);
+            this.Asal.TabIndex = 0;
+            this.Asal.TabStop = false;
+            // 
+            // Hasil
+            // 
+            this.Hasil.Location = new System.Drawing.Point(318, 12);
+            this.Hasil.Name = "Hasil";
+            this.Hasil.Size = new System.Drawing.Size(300, 300);
+            this.Hasil.TabIndex = 1;
+            this.Hasil.TabStop = false;
+            // 
+            // pilihGambar
+            // 
+            this.pilihGambar.Location = new System.Drawing.Point(12, 318);
+            this.pilihGambar.Name = "pilihGambar";
+            this.pilihGambar.Size = new System.Drawing.Size(98, 23);
+            this.pilihGambar.TabIndex = 2;
+            this.pilihGambar.Text = "Select Photo";
+            this.pilihGambar.UseVisualStyleBackColor = true;
+            this.pilihGambar.Click += new System.EventHandler(this.pilihGambar_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 386);
+            this.Controls.Add(this.pilihGambar);
+            this.Controls.Add(this.Hasil);
+            this.Controls.Add(this.Asal);
+            this.Name = "Main";
+            this.Text = "Main";
+            ((System.ComponentModel.ISupportInitialize)(this.Asal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Hasil)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Asal;
+        private System.Windows.Forms.PictureBox Hasil;
+        private System.Windows.Forms.Button pilihGambar;
+    }
+}
+
---
+++ FaceDetection/Main.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FaceDetection
+{
+    public partial class Main : Form
+    {
+        Image GambarAsli;
+        string PathGambar = "";
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void pilihGambar_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Pilih = new OpenFileDialog();
+            Pilih.FileName = "";
+            Pilih.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+            Pilih.ShowDialog();
+            PathGambar = Pilih.FileName;
+            if (PathGambar != "")
+            {
+                GambarAsli = Image.FromFile(PathGambar);
+                Asal.Image = GambarAsli;
+                Asal.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+        }
+    }
+}
